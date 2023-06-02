1c775aade20a63860ef46e8776356b5b364fd098
patilpiya@gmail.com <patilpiya6692@gmail.com>
2023-06-02 15:19:39 +0530

    my changes

--- MY_REPOSITRY_PRACTICE/src/app/app.component.ts
+++ MY_REPOSITRY_PRACTICE/src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { BindingPipe } from '@angular/compiler';
 import { Component } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
@@ -8,3 +11,37 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'MY_REPOSITRY_PRACTICE';
 }
+//propertyName :datatype = value/data;
+ //testcase1  
+  firstname : string = 'piya';  
+
+ //testcase 2
+  firstName1 = 'poonam';
+  firstName2! : string  ;
+ surName2 : any;
+ surName : any = true;
+ formTitle : string = 'Login Form...';
+ studentName = 'Vishal';
+ amount = '5600';
+ isFavCity = false;
+ myName = "Om";
+ gender = false;
+ isDisabled = true;
+ eventBindingData = 60000;
+ twoWayBindingTest = 96565;
+
+  testingInterpolation( x : number, y : number ){
+ 
+     return x + y;
+ }
+
+ testingEventBinding(){
+   this.eventBindingData = 50000;
+   console.log(this.eventBindingData);
+   
+ }
+ test(){
+   console.log(this.twoWayBindingTest);
+ }
+}
+
